0ed154322359748a508136130c298f48014ba3e0
PP Laptop <steven@developersteve.com>
2015-04-11 12:49:03 +1000

    initial

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'braintree'
+require 'awesome_print'
+
+  Braintree::Configuration.environment = :sandbox
+  Braintree::Configuration.merchant_id = 'ffdqc9fyffn7yn2j'
+  Braintree::Configuration.public_key = 'qj65nndbnn6qyjkp'
+  Braintree::Configuration.private_key = 'a3de3bb7dddf68ed3c33f4eb6d9579ca'
+
+  get '/' do
+    @client_token = Braintree::ClientToken.generate
+    erb :index
+  end
+
+  post '/process' do
+
+    result = Braintree::Transaction.sale(
+      amount: params[:amount],
+      payment_method_nonce: params[:payment_method_nonce]
+    )
+
+    if result.success?
+      @transaction = result.transaction
+      erb :process
+    else
+      'Payment failed'
+    end
+
+  end
